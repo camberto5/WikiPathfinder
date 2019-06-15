95d10adc71b6430b8431cf24f4fed59d3055e0e5
Cameron  Maberto <cmaberto@uci.edu>
2019-06-14 22:43:36 -0700

    Testing cLion git commit

---
+++ cmake-build-debug/CMakeFiles/WikiPathfinder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WikiPathfinder.dir/main.cpp.o"
+  "CMakeFiles/WikiPathfinder.dir/HTTPDownloader.cpp.o"
+  "CMakeFiles/WikiPathfinder.dir/Queue.cpp.o"
+  "WikiPathfinder.pdb"
+  "WikiPathfinder"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WikiPathfinder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
